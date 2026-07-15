b27ddfbf6089fd4888a9888424040b0e0dcfdf17
Andreas Schempp <andreas.schempp@terminal42.ch>
2026-07-15 13:41:26 +0200

    Migrate all fields in a table at the same time

--- core-bundle/src/Migration/Version600/AbstractColumnToVirtualMigration.php
+++ core-bundle/src/Migration/Version600/AbstractColumnToVirtualMigration.php
@@ -16,6 +16,7 @@ use Contao\CoreBundle\Migration\AbstractMigration;
 use Contao\CoreBundle\Migration\MigrationResult;
 use Contao\StringUtil;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Types\Types;
 
 abstract class AbstractColumnToVirtualMigration extends AbstractMigration
 {
@@ -52,37 +53,53 @@ abstract class AbstractColumnToVirtualMigration extends AbstractMigration
 
         foreach ($mapping as $table => $fields) {
             // Ignore tables that do not exist
-            if (!$schemaManager->tablesExist([$table])) {
+            if (!$schemaManager->tableExists($table)) {
                 continue;
             }
 
             $tableDefinition = $schemaManager->introspectTableByUnquotedName($table);
+            $fields = array_filter($fields, static fn ($targetColumn, $field) => $tableDefinition->hasColumn($field), ARRAY_FILTER_USE_BOTH);
 
-            foreach ($fields as $field => $targetColumn) {
-                if (!$tableDefinition->hasColumn($field)) {
-                    continue;
-                }
+            // If there is at least one column that should be a virtual field, run the migration
+            if ([] === $fields) {
+                continue;
+            }
 
-                $rows = $this->connection->fetchAllAssociative("SELECT id, `$field`, `$targetColumn` FROM `$table`");
+            $missingTargetColumns = array_filter(array_unique(array_values($fields)), static fn (string $targetColumn) => !$tableDefinition->hasColumn($targetColumn));
 
-                foreach ($rows as $row) {
-                    if (null === $row[$targetColumn]) {
-                        $jsonData = [];
+            if ([] !== $missingTargetColumns) {
+                // Let schema migrations handle the correct column type, it can always convert TEXT to JSON if supported
+                $this->connection->executeStatement("ALTER TABLE $table ADD COLUMN `".implode('` text NULL, ADD COLUMN `', $missingTargetColumns)."` text NULL");
+            }
+
+            $rows = $this->connection->fetchAllAssociative("SELECT * FROM `$table`");
+
+            foreach ($rows as $row) {
+                $jsonData = [];
+
+                foreach ($fields as $field => $targetColumn) {
+                    if (null === ($row[$targetColumn] ?? null)) {
+                        $jsonData[$targetColumn] = [];
                     } else {
-                        $jsonData = json_decode($row[$targetColumn], true, flags: JSON_THROW_ON_ERROR);
+                        $jsonData[$targetColumn] = json_decode($row[$targetColumn], true, flags: JSON_THROW_ON_ERROR);
                     }
 
-                    if (isset($jsonData[$field])) {
+                    if (isset($jsonData[$targetColumn][$field])) {
                         continue;
                     }
 
-                    $jsonData[$field] = StringUtil::ensureStringUuids($row[$field]);
-
-                    $this->connection->update($table, [$targetColumn => json_encode($jsonData, flags: JSON_THROW_ON_ERROR)], ['id' => $row['id']]);
+                    $jsonData[$targetColumn][$field] = StringUtil::ensureStringUuids($row[$field]);
                 }
 
-                $this->connection->executeQuery("ALTER TABLE `$table` DROP COLUMN `$field`");
+                $this->connection->update(
+                    $table,
+                    array_map(static fn (array $json) => json_encode($json, flags: JSON_THROW_ON_ERROR), $jsonData),
+                    ['id' => $row['id']],
+                    array_map(static fn () => Types::JSON, $jsonData),
+                );
             }
+
+            $this->connection->executeQuery("ALTER TABLE `$table` DROP COLUMN `".implode('`, DROP COLUMN `', array_keys($fields)).'`');
         }
 
         return $this->createResult(true);
